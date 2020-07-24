cf61a8e52e9584471721d9690775c3e64fe6a312
OkelloAndrewPeters <okelloan@gmail.com>
2020-07-24 23:52:56 +0300

    begin commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import Header from './components/layout/header';
 import './App.css';
+import AddTodo from './components/AddTodo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ state = {
+   todos: [
+    {
+      id: 1,
+      title: 'church tomorrow',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'meeting with boss',
+      completed: false
+    },
+    {
+      id: 3,
+      title: 'uploading video on channel',
+      completed: false
+    },
+   ]
+  
+ }
+
+  markComplete = (id) => {
+    this.setState ({ todos: this.state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      } return todo;
+    }) });
+  }
+
+  delTodo = (id) => {
+      this.setState({ todos:[...this.state.todos.filter(todo => todo.id
+        !== id)] })
+  }
+
+
+  
+  addTodo = (title) => {
+    const newTodo = {
+          id: 4,
+          title,
+          completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
+
+  render () {
+
+    return (
+      <div className='App'>
+        <div className= 'container'>
+          <Header />
+          <AddTodo addTodo = { this.addTodo } />
+          <Todos todos = { this.state.todos} 
+          markComplete = {this.markComplete} 
+          delTodo = { this.delTodo } />
+        </div>
+          
+      </div>
+    );
+  }
 }
 
+
 export default App;
+
